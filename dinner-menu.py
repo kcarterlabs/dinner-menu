628ef3dceca5bd41626febc37d6ca51e956ed862
Kenneth Carter <kennycocopuff@gmail.com>
2025-06-14 18:36:00 -0700

    adding code

---
+++ dinner-menu.py
@@ -0,0 +1,50 @@
+import requests
+import time
+from datetime import datetime
+
+def forecast(date_range):
+  print("gathering intel from Big Weather")
+  time.sleep(1)
+  print("Cracking the eggs on your {date_range} forecast!")
+  url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
+  querystring = {"q":"Spokane","days":date_range} 
+  headers = {
+  	"x-rapidapi-key": "e83f4ef893mshde26f360b6b202ep14d34fjsnc0591e58501d",
+  	"x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
+  }
+  
+  response = requests.get(url, headers=headers, params=querystring)
+  data = response.json()
+  city = data["location"]["name"]
+  region = data["location"]["region"]
+  
+  print(f"Weather for {city} {region}!")
+  formatted = [
+    f"{datetime.strptime(day['date'], '%Y-%m-%d').strftime('%A')}: {day['day']['maxtemp_f']}°F"
+    for day in data["forecast"]["forecastday"]
+  ]
+  
+  print(formatted)
+  return formatted
+  
+##############################
+##############DEBUG###############
+# print(f"Welcome to the Dinner Menu, how many days are you looking to search? (Up to 14)")
+# date_range = input("how many days do you want to search for?: ")
+# forecast(date_range)
+##################################]
+##################################
+###################################
+
+while True: 
+  print(f"Welcome to the Dinner Menu, how many days are you looking to search? (Up to 14)")
+
+  ####TODO add option block that helps navigate the app and functions
+#   option = input("Options: ")
+#   option = int(option)
+
+  date_range = input("how many days do you want to search for?: ")
+  if int(date_range) <= 14: 
+    forecast(date_range)
+  else:
+    print(". Fourteen shalt be the highest number thou shalt enter, and the number of thy entering shalt be fourteen or less . fifteen shalt thou not enter")
